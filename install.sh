9313b4f811ad92d777a10ca418a7e8fa4aca9955
zxyxx <379951650@qq.com>
2018-06-17 12:24:46 +0800

    [+] add install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 # lxss的安装脚本，需要配置django的secret_key，登陆密码，部署的 url:port，更改 data 目录的读写权限
 # 把 backend/static/login 复制到 frontend/static/ 中，因为如果用 nginx 反代理静态文件只能选择一个目录
 
-echo "[!] Welcome to Lnyas's XSS platform"
-echo "[!] If you want to Please see READNE.md "
-echo "[!] chmod and copy static file..."
+echo "[!] Welcome to Lnyas's XSS platform ..."
+echo "[!] If you want to know more about install, please see READNE.md ..."
+echo "[!] start install ..."
+echo "[!] chmod and copy static file ..."
 chmod -R 777 backend/data/
 chmod -R 777 frontend/dist/static
 
