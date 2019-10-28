8f9151514fa58aaabe7623ba3cea595b716c756a
zxyxx <379951650@qq.com>
2019-10-28 17:05:25 +0800

    update

---
+++ conf.d
@@ -0,0 +1,76 @@
+user  www www;
+
+worker_processes auto;
+
+error_log  /home/wwwlogs/nginx_error.log  crit;
+
+pid        /usr/local/nginx/logs/nginx.pid;
+
+#Specifies the value for maximum file descriptors that can be opened by this process.
+worker_rlimit_nofile 51200;
+
+events
+    {
+        use epoll;
+        worker_connections 51200;
+        multi_accept on;
+    }
+
+http
+    {
+        include       mime.types;
+        default_type  application/octet-stream;
+
+        server_names_hash_bucket_size 128;
+        client_header_buffer_size 32k;
+        large_client_header_buffers 4 32k;
+        client_max_body_size 50m;
+
+        sendfile   on;
+        tcp_nopush on;
+
+        keepalive_timeout 60;
+
+        tcp_nodelay on;
+
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+        fastcgi_buffer_size 64k;
+        fastcgi_buffers 4 64k;
+        fastcgi_busy_buffers_size 128k;
+        fastcgi_temp_file_write_size 256k;
+
+        gzip on;
+        gzip_min_length  1k;
+        gzip_buffers     4 16k;
+        gzip_http_version 1.1;
+        gzip_comp_level 2;
+        gzip_types     text/plain application/javascript application/x-javascript text/javascript text/css application/xml application/xml+rss;
+        gzip_vary on;
+        gzip_proxied   expired no-cache no-store private auth;
+        gzip_disable   "MSIE [1-6]\.";
+
+        #limit_conn_zone $binary_remote_addr zone=perip:10m;
+        ##If enable limit_conn_zone,add "limit_conn perip 10;" to server section.
+
+        server_tokens off;
+        access_log off;
+
+server {
+    listen 80 default_server;
+    #listen [::]:80;
+    server_name _;
+    location ^~ /static {
+        alias /var/www/frontend/dist/static;
+    }
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_pass_header       Authorization;
+        proxy_pass_header       WWW-Authenticate;
+        proxy_set_header Host $host;
+        proxy_set_header Cookie $http_cookie;
+        proxy_set_header X-Forwarded-For $remote_addr;
+    }
+}
